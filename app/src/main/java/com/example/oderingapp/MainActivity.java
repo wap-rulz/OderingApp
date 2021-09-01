22c0edc541da64da2509b7fc675673dbb453aff3
Ashen Thiwanka <ashenthiwanka1999@outlook.com>
2021-09-01 18:30:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/oderingapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.oderingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openpage2();
+            }
+        });
+    }
+    public void openpage2()
+    {
+        Intent intent = new Intent(this, page2.class);
+        startActivity(intent);
+
+    }
+}
